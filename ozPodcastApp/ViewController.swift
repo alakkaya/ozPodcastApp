fb78250ac5fa07e55670cfa05f04518e145b8dcb
Ali Akkaya <117384310+alakkaya@users.noreply.github.com>
2025-01-18 10:52:36 +0300

    Initial Commit

---
+++ ozPodcastApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ozPodcastApp
+//
+//  Created by Ali Akkaya on 18.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
